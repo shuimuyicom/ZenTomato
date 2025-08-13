ec326d48c165b666626280c4a0ff0433ebab6b06
shuimuyicom <shuimuyi@example.com>
2025-08-13 15:25:29 +0800

    Initial Commit

---
+++ ZenTomato/ZenTomatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenTomatoApp.swift
+//  ZenTomato
+//
+//  Created by Ban on 2025/8/13.
+//
+
+import SwiftUI
+
+@main
+struct ZenTomatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZenTomatoTests/ZenTomatoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZenTomatoTests.swift
+//  ZenTomatoTests
+//
+//  Created by Ban on 2025/8/13.
+//
+
+import Testing
+@testable import ZenTomato
+
+struct ZenTomatoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
